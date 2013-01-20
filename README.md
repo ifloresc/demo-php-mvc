4e274ea05bda3b74f7c6f1ac72573163656ebc12
Ivan A. Flores C <ifloresenator@gmail.com>
2013-01-20 12:52:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+php-core
+========
+
+MVC Php
+
+Se esta trabajando en la estructura para manejar como libreria, para los proyectos php
+
+version 0.0.1
+
+# Uso
+
+
+
+
+Autor : Ivan A. Flores Correa
+Email : ifloresenator@gmail.com
---
+++ src/views/index.php
@@ -0,0 +1,13 @@
+Ejemplos se uso de esta libreria :
+
+<ul>
+	<li>
+		<a href="<?php echo $site; ?>test/simple">Simple</a>
+	</li>
+	<li>
+		<a href="<?php echo $site; ?>test/access">Validar Acceso</a>
+	</li>
+	<li>
+		<a href="<?php echo $site; ?>test/own">Only Own</a>
+	</li>
+</ul>
