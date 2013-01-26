6e0c4d7c1f1ec280bb6e51158b292a4fd64417a2
Ivan A. Flores C <ifloresenator@gmail.com>
2013-01-26 00:06:44 -0300

    Se agregan nuevos ejemplos

---
+++ src/controller/test/errortestController.php
@@ -0,0 +1,20 @@
+<?php
+
+class errortestController extends CrudController {
+	
+	public function crudAction() {
+		
+		throw new Exception("Error en la Logica");
+	}
+	
+	public function getMsg() {
+		return "Succes";
+	}
+
+	protected function accessControl() {
+		return false;
+	}
+	
+}
+
+?>
