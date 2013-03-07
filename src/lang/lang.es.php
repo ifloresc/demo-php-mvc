0b37f01799e1c9b9f43e31f254ab2dc175733cd3
Ivan A. Flores C <ifloresenator@gmail.com>
2013-03-07 17:10:23 -0300

    Se agregan cambios, por cambio de version de libreria

---
+++ src/lang/lang.es.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+---------------------------------------------------
+Language : Español
+---------------------------------------------------
+ */
+$lang = array();
+
+$lang['PAGE_NAME'] = 'PHP MVC DEMO';
+$lang['PAGE_TITLE'] = 'PHP MVC TEST';
+
+$lang['FOOTER_SITE'] = 'PHP MVC DEMO';
+// Paginas
+$lang['INIT_TITLE'] = 'Bienvenido';
+
+?>
