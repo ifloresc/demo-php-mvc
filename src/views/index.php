f8a73d8ac63b93f401d4a8c7b2c6d1794d116294
Ivan A. Flores C <ivan.flores@excelsys.net>
2013-03-19 11:17:26 -0300

    Se arregla despliegue de informacion de ejemplos

--- src/views/index.php
+++ src/views/index.php
@@ -2,27 +2,107 @@ Ejemplos se uso de esta libreria :
 
 <ul>
 	<li>
-		<a href="<?php echo $site; ?>test/simple">Simple</a>
+        <article>
+            <header>
+                <h3>Simple</h3>
+                <h1>Implementacion de una Controlador que retorna una pagina</h1>
+            </header>
+            <section id="description"></section>
+            <section id="code">aaa</section>
+            <footer>
+                <a href="<?php echo $site; ?>test/simple">Ver Demo</a>
+            </footer>
+        </article>
 	</li>
 	<li>
-		<a href="<?php echo $site; ?>test/access">Validar Acceso</a>
+        <article>
+            <header>
+                <h3>Validar Acceso</h3>
+                <h1>Implementacion de una Controlador con Acceso Requerido</h1>
+            </header>
+            <section id="description"></section>
+            <section id="code">aaa</section>
+            <footer>
+                <a href="<?php echo $site; ?>test/access">Ver Demo</a>
+            </footer>
+        </article>
 	</li>
 	<li>
-		<a href="<?php echo $site; ?>test/own">Only Own</a>
+        <article>
+            <header>
+                <h3>Only Own</h3>
+                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+            </header>
+            <section id="description"></section>
+            <section id="code">aaa</section>
+            <footer>
+                <a href="<?php echo $site; ?>test/own">Ver Demo</a>
+            </footer>
+        </article>
 	</li>
 	<li>
-		<a url="<?php echo $site; ?>test/error" id="btn-ajax">Error</a>
+        <article>
+            <header>
+                <h3>Error</h3>
+                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+            </header>
+            <section id="description"></section>
+            <section id="code">aaa</section>
+            <footer>
+                <a url="<?php echo $site; ?>test/error" id="btn-ajax">Ver Demo</a>
+            </footer>
+        </article>
 	</li>
 	<li>
-		<a url="<?php echo $site; ?>test/succes" id="btn-ajax">Succes</a>
+        <article>
+            <header>
+                <h3>Succes</h3>
+                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+            </header>
+            <section id="description"></section>
+            <section id="code">aaa</section>
+            <footer>
+                <a url="<?php echo $site; ?>test/succes" id="btn-ajax">Ver Demo</a>
+            </footer>
+        </article>
 	</li>
 	<li>
-		<a href="<?php echo $site; ?>test/json">JSON</a>
+        <article>
+            <header>
+                <h3>JSON</h3>
+                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+            </header>
+            <section id="description"></section>
+            <section id="code">aaa</section>
+            <footer>
+                <a href="<?php echo $site; ?>test/json">Ver Demo</a>
+            </footer>
+        </article>
 	</li>
 	<li>
-		<a href="<?php echo $site; ?>test/export">Export</a>
+        <article>
+            <header>
+                <h3>Export</h3>
+                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+            </header>
+            <section id="description"></section>
+            <section id="code">aaa</section>
+            <footer>
+                <a href="<?php echo $site; ?>test/export">Ver Demo</a>
+            </footer>
+        </article>
 	</li>
 	<li>
-                <a href="<?php echo $site; ?>test/load/1">Get ID</a>
-        </li>
+        <article>
+            <header>
+                <h3>Get ID</h3>
+                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+            </header>
+            <section id="description"></section>
+            <section id="code">aaa</section>
+            <footer>
+                <a href="<?php echo $site; ?>test/load/1">Ver Demo</a>
+            </footer>
+        </article>
+    </li>
 </ul>
