a16dcdee5ad77bbb78539e873c3cda2044f7e14f
Ivan A. Flores C <ivan.flores@excelsys.net>
2013-03-21 15:40:37 -0400

    Se estructura

--- src/views/index.php
+++ src/views/index.php
@@ -13,14 +13,13 @@
         <article>
             <header>
                 <h3>Simple</h3>
-                <h1>Implementacion de una Controlador que retorna una pagina</h1>
+                <h1>/src/controller/test/simpletestController.php</h1>
             </header>
-            <section id="description"></section>
+            <section id="description">
+		En este ejemplo se genera un Controlador que solo retorna una pagina.
+	    </section>
             <section id="code">
-		private function foo(items) {
-		    var x = "All this is syntax highlighted";
-		    return x;
-		}
+aa
 	    </section>
             <footer>
                 <a href="<?php echo $site; ?>test/simple">Ver Demo</a>
@@ -31,14 +30,11 @@
         <article>
             <header>
                 <h3>Validar Acceso</h3>
-                <h1>Implementacion de una Controlador con Acceso Requerido</h1>
+                <h1>/src/controller/test/accesstestController.php</h1>
             </header>
-            <section id="description"></section>
+            <section id="description">Implementacion de una Controlador con Acceso Requerido</section>
             <section id="code">
-		function foo(items) {
-		    var x = "All this is syntax highlighted";
-		    return x;
-		}</section>
+		aa</section>
             <footer>
                 <a href="<?php echo $site; ?>test/access">Ver Demo</a>
             </footer>
@@ -48,14 +44,11 @@
         <article>
             <header>
                 <h3>Only Own</h3>
-                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+                <h1>/src/controller/test/owntestController.php</h1>
             </header>
-            <section id="description"></section>
+            <section id="description">Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</section>
             <section id="code">
-		function foo(items) {
-		    var x = "All this is syntax highlighted";
-		    return x;
-		}</section>
+		aa</section>
             <footer>
                 <a href="<?php echo $site; ?>test/own">Ver Demo</a>
             </footer>
@@ -65,9 +58,9 @@
         <article>
             <header>
                 <h3>Error</h3>
-                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+                <h1>/src/controller/test/errortestController.php</h1>
             </header>
-            <section id="description"></section>
+            <section id="description">Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</section>
             <section id="code">aaa</section>
             <footer>
                 <a url="<?php echo $site; ?>test/error" id="btn-ajax">Ver Demo</a>
@@ -78,9 +71,9 @@
         <article>
             <header>
                 <h3>Succes</h3>
-                <h1>Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</h1>
+                <h1>/src/controller/test/succestestController.php</h1>
             </header>
-            <section id="description"></section>
+            <section id="description">Implementacion de una Controlador con Validacion de que la data que vea sea solo de un dusño definido</section>
             <section id="code">aaa</section>
             <footer>
                 <a url="<?php echo $site; ?>test/succes" id="btn-ajax">Ver Demo</a>
@@ -130,7 +123,7 @@
 <!--
 <script src="http://d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
 <script>
-    var editor = ace.edit("editor");
+    var editor = ace.edit("code");
     editor.setTheme("ace/theme/eclipse");
     editor.getSession().setMode("ace/mode/php");
     editor.setReadOnly(true);
